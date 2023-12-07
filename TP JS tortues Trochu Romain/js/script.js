e0c71d76713c46e6f7fe10c89534c546bf17081a
Romain Trochu <romain.trochu@gmail.com>
2023-12-07 09:49:00 +0100

    end

--- TP JS tortues Trochu Romain/js/script.js
+++ TP JS tortues Trochu Romain/js/script.js
@@ -1,4 +1,4 @@
-// created div with overlay & img empty
+// created div with overlay & empty img
 
 let zoomCards = document.createElement('div');
 zoomCards.classList.add('overlay');
@@ -7,9 +7,9 @@ let zoomTurtle = document.createElement('img');
 // function display turtle big size
 
 function displayTurtle(dataname){
-    // introduced div at first div
+    // introduction div at first div
     document.querySelector('#main-meet').prepend(zoomCards);
-    // introduced data-name of one img to display the right img
+    // introduction data-name of one img to display the right img
     zoomTurtle.setAttribute('src', './assets/zoom-turles/turtle-' + dataname + '.png');
     // add img to display big size img to div recently create
     zoomCards.append(zoomTurtle);
@@ -21,18 +21,22 @@ function removeTurtle(param){
     param.remove();
 }
 
+// get content-form to add after messageErrorContainer;
 let field = document.querySelector(`#content-form`);
 let inputEmail = document.querySelector('#newsletter');
 let inputSubmit = document.querySelector('input[type=submit]');
 
 function errors(param1){
+
     // add class on input to display errors
     inputEmail.classList.add('input-error-email')
     inputSubmit.classList.add('input-error-btn')
+
     // create div to display error message
     let messageErrorContainer = document.createElement('div');
     messageErrorContainer.classList.add('text-error');
     let messageError = messageErrorContainer.appendChild(document.createElement('p'));
+
     // error message is in param to do modify easier & adapt message of type of error
     messageError.textContent = param1;
     field.after(messageErrorContainer);
@@ -51,10 +55,15 @@ function removeError(){
 // function valid to display a congratulations message & remove input newsletter
 
 function valid(){
+    // create div to display valid message
     let messageValidContainer = document.createElement('div');
     let messsageValid = messageValidContainer.appendChild(document.createElement('p'));
-    messsageValid.classList.add('valid')
+
+    // add class valid to div & p
+    messsageValid.classList.add('valid');
     messsageValid.classList.add('message-valid');
+    // content valid message
+
     messsageValid.textContent = 'Vous avez bien été inscrits à la newsletter !';
     field.after(messageValidContainer);
     field.remove();
